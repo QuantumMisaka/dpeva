810fc01d50f0c4f3fda5c4129f0a5541bf200e1e
JamesMisaka <ff6757442@163.com>
2025-01-16 12:04:55 +0800

    update workflow

--- utils/rnd_iterative_select.py
+++ utils/rnd_iterative_select.py
@@ -26,15 +26,13 @@ data_pool_path = "data-val-stage1"
 dpdata_string = "O*"
 
 # setting of rnd
-output_dim = 240  # output dimension
-hidden_dim = 240  # hidden dimension
+output_dim = 80  # output dimension
+hidden_dim = 160  # hidden dimension
 num_residual_blocks = 2  # number of residual blocks
 device = "cuda"  # device
 num_iter = 100  # number of selection iterations
 sample_per_iter = 1  # number of samples per iteration
 train_mask = 0.4  # mask ratio for training data
-select_enhance = 100  # enhance the selected data by a factor
-enhance_decay = 0.3  # decay factor for enhance
 
 
 # read descriptors/*/desc.npy data
@@ -85,7 +83,7 @@ for lbsys in dpdata_pool:
 input_dim = desc_trn[0].shape[-1]  # input dimension
 rnd = RandomNetworkDistillation(
             input_dim=input_dim, 
-            output_dim=input_dim, 
+            output_dim=output_dim, 
             hidden_dim=hidden_dim, 
             num_residual_blocks=num_residual_blocks, 
             distance_metric="cossim", 
@@ -97,8 +95,8 @@ select_index = np.array([], dtype=int)
 
 for iter_ind in range(num_iter):
     logger.info(f"Selecting data point {iter_ind + 1}/{num_iter}...")
-    num_batches = int(np.sqrt(128 / (iter_ind + 1)) * 1000)  # number of batches 
-    decay_steps = num_batches // 200  # decay steps
+    num_batches = int(np.sqrt(256 / (iter_ind + 1)) * 500)  # number of batches 
+    decay_steps = num_batches // 100  # decay steps
     
     # dataset process and selection
     train_array = np.concatenate(desc_trn, axis=0)
@@ -109,13 +107,7 @@ for iter_ind in range(num_iter):
     if len(select_index) == 0:
         train_desc = train_desc_raw.copy()
     else:
-        pool_desc_added_list = []
-        for num_ind, select_ind in enumerate(select_index):
-            # let the enhance weight downgrade by exponential rate
-            enhance_weight = int(np.max((select_enhance * np.exp(
-                - (1 - enhance_decay) * (len(select_index) - num_ind - 1)), 1)))
-            pool_desc_added_list.extend([desc_pool[int(select_ind)]] * enhance_weight)
-        pool_desc_added = np.concatenate(pool_desc_added_list, axis=0)
+        pool_desc_added = np.concatenate([desc_pool[int(i)] for i in select_index], axis=0)
         train_desc = np.concatenate([train_desc_raw, pool_desc_added], axis=0)
     # generate target desc data which purge the selected data points
     # mean pooling in target_desc_list in natom dimension
@@ -126,12 +118,12 @@ for iter_ind in range(num_iter):
     
     rnd.train(train_desc, 
           num_batches=num_batches,
-          batch_size=2048,
+          batch_size=4096,
           initial_lr=1e-3,
           gamma=0.95,
           decay_steps=decay_steps,
-          disp_freq=1000,
-          save_freq=4000)
+          disp_freq=500,
+          save_freq=2000)
     
     # calculate intrinsic reward
     intrinsic_rewards = rnd.eval_intrinsic_rewards(pool_desc_stru, batch_size=4096, disp_freq=1)
@@ -153,19 +145,16 @@ for iter_ind in range(num_iter):
     plt.grid(True)
     plt.savefig(f"{intrinsic_rewards_savedir}/rewards_{iter_ind}.png", dpi=100)
     plt.close() # close the plot
-    
+
     # select data points by the maximum intrinsic reward
     select_ind_iter = np.argsort(intrinsic_rewards)[-sample_per_iter:]
-    # reverse the order to make the last one has the highest intrinsic reward
-    select_ind_iter = select_ind_iter[::-1]
-    
+
     logger.info(f"Selected data index: {select_ind_iter} with intrinsic rewards: {intrinsic_rewards[select_ind_iter]}, which has the system key {[desc_pool_keys[ind] for ind in select_ind_iter]}")
-    
+
     # add the selected index to the selected index array
     select_index = np.concatenate((select_index, select_ind_iter))
-    
-    
-    
+
+
 # save the selected data indice
 select_index_array = np.array(select_index)
 save_file = "selected_index.npy"
@@ -177,8 +166,7 @@ logger.info("PCA reduction for visualization...")
 pca = PCA(n_components=2) 
 desc_trn_stru = np.concatenate([np.mean(desc, axis=0).reshape(1,desc.shape[-1]) for desc in desc_trn], axis=0)
 desc_pool_stru = np.concatenate([np.mean(desc, axis=0).reshape(1,desc.shape[-1]) for desc in desc_pool], axis=0)
-desc_all_stru = np.concatenate((desc_trn_stru, desc_pool_stru), axis=0)
-pca_result = pca.fit_transform(desc_all_stru) # PCA reduction for all data points
+pca_result = pca.fit_transform(desc_trn_stru) # PCA reduction for all data points
 trn_points = pca.transform(desc_trn_stru) 
 pool_points = pca.transform(desc_pool_stru) 
 select_points = pca.transform([desc_pool_stru[i] for i in range(len(desc_pool_stru)) if i in select_index])
@@ -214,9 +202,14 @@ logger.info(f"PCA visualization saved to {pca_fig_name}")
 # choose the selected data points in corresponding dpdata
 logger.info("Saving selected data points in dpdata format...")
 selected_dpdata = dpdata.MultiSystems()
+selected_string = ""
 for i in select_index:
     keyname, ind = desc_pool_keys[i].split("-")
+    # save keyname-ind pairs
+    selected_string += f"{keyname}-{ind}\n"
     selected_dpdata.append(dpdata_pool_dict[keyname][int(ind)])
-selected_dpdata.to("selected_data", fmt="deepmd/npy")
+with open("selected_data.txt", "w") as f:
+    f.write(selected_string)
+selected_dpdata.to_deepmd_npy("selected_data")
 
 logger.info("DONE!")
