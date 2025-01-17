872773df5a0de58caf35efd0b9ad8eee82a6670a
JamesMisaka <ff6757442@gmail.com>
2025-01-17 19:29:29 +0800

    update selection

--- utils/direct-rnd_iter-select.py
+++ utils/direct-rnd_iter-select.py
@@ -10,6 +10,7 @@ import os
 import logging
 
 from dpeva.uncertain.rnd import RandomNetworkDistillation
+from dpeva.sampling.direct import BirchClustering, DIRECTSampler, SelectKFromClusters
 from sklearn.decomposition import PCA
 
 logging.basicConfig(
@@ -43,6 +44,9 @@ rnd_train_decay_ratio = 100
 rnd_train_disp_freq = 500
 rnd_train_save_freq = 2000
 rnd_eval_disp_freq = 1
+direct_cluster_ratio = 0.2
+direct_k = 2
+direct_thr_init = 0.05
 
 
 # read descriptors/*/desc.npy data
@@ -101,11 +105,24 @@ rnd = RandomNetworkDistillation(
 
 # iteratively use RND to choose the next data point
 # maintain a selected index for the selected data points
-select_index = np.array([], dtype=int)
+RND_global_select_indices = np.array([], dtype=int)
+RND_local_select_indices = np.array([], dtype=int)
 
-# mean pooling in target_desc_list in natom dimension
+# mean pooling in pool_desc_list in natom dimension
 pool_desc_stru_list = [np.mean(desc, axis=0).reshape(1,desc.shape[-1]) for desc in desc_pool]
 pool_desc_stru = np.concatenate(pool_desc_stru_list, axis=0)
+
+# direct selection for pool desc
+num_stru_pool = len(pool_desc_stru)
+DIRECT_sampler = DIRECTSampler(
+    structure_encoder=None,
+    clustering=BirchClustering(n=int(num_stru_pool * 0.2), threshold_init=0.05),
+    select_k_from_clusters=SelectKFromClusters(k=2),
+)
+DIRECT_selection = DIRECT_sampler.fit_transform(pool_desc_stru)
+DIRECT_selected_indices = DIRECT_selection["selected_indexes"]
+pool_desc_stru_selected = pool_desc_stru[DIRECT_selected_indices] 
+# the order for DIRECT_select_indices and pool_desc_stru_selected are the same
 
 for iter_ind in range(num_iter):
     logger.info(f"Selecting data point {iter_ind + 1}/{num_iter}...")
@@ -118,15 +135,15 @@ for iter_ind in range(num_iter):
         range(len(train_array)), int((1-train_mask)*len(train_array)), replace=False)
     train_desc_raw = train_array[train_index]
     # add the selected data points to the training set
-    if len(select_index) == 0:
+    if len(RND_global_select_indices) == 0:
         train_desc = train_desc_raw.copy()
     else:
-        pool_desc_added = np.concatenate([desc_pool[int(i)] for i in select_index], axis=0)
+        pool_desc_added = np.concatenate([desc_pool[int(i)] for i in RND_global_select_indices], axis=0)
         train_desc = np.concatenate([train_desc_raw, pool_desc_added], axis=0)
     # generate target desc data which purge the selected data points
 
     
-    logger.info(f"Training data shape: {train_desc.shape}, target data shape: {pool_desc_stru.shape}")
+    logger.info(f"Training data shape: {train_desc.shape}, target data shape: {pool_desc_stru_selected.shape}")
     
     rnd.train(train_desc, 
           num_batches=num_batches,
@@ -139,13 +156,12 @@ for iter_ind in range(num_iter):
     
     # calculate intrinsic reward
     intrinsic_rewards = rnd.eval_intrinsic_rewards(
-                    pool_desc_stru, 
+                    pool_desc_stru_selected, 
                     batch_size=rnd_eval_batchsize, 
                     disp_freq=rnd_eval_disp_freq)
     
     # mask the selected data points
-    
-    intrinsic_rewards[list(select_index)] = 0 # or -np.inf
+    intrinsic_rewards[list(RND_local_select_indices)] = 0 # or -np.inf
     
     # plot the distribution of intrinsic rewards
     intrinsic_rewards_savedir = "rnd_rewards"
@@ -161,17 +177,19 @@ for iter_ind in range(num_iter):
     plt.savefig(f"{intrinsic_rewards_savedir}/rewards_{iter_ind}.png", dpi=100)
     plt.close() # close the plot
 
-    # select data points by the maximum intrinsic reward
-    select_ind_iter = np.argsort(intrinsic_rewards)[-sample_per_iter:]
+    # select data points by the maximum intrinsic reward, give local indices
+    select_ind_iter_local = np.argsort(intrinsic_rewards)[-sample_per_iter:]
+    select_ind_iter_global = DIRECT_selected_indices[select_ind_iter_local]
 
-    logger.info(f"Selected data index: {select_ind_iter} with intrinsic rewards: {intrinsic_rewards[select_ind_iter]}, which has the system key {[desc_pool_keys[ind] for ind in select_ind_iter]}")
+    logger.info(f"Selected data index: {select_ind_iter_local}/{select_ind_iter_global} in DIRECT_sampled/raw pool data with intrinsic rewards: {intrinsic_rewards[select_ind_iter_local]}, which has the system key {[desc_pool_keys[ind] for ind in select_ind_iter_global]}")
 
     # add the selected index to the selected index array
-    select_index = np.concatenate((select_index, select_ind_iter))
+    RND_local_select_indices = np.concatenate((RND_local_select_indices, select_ind_iter_local))
+    RND_global_select_indices = np.concatenate((RND_global_select_indices, select_ind_iter_global))
 
 
 # save the selected data indice
-select_index_array = np.array(select_index)
+select_index_array = np.array(RND_global_select_indices)
 save_file = "selected_index.npy"
 np.save(save_file, select_index_array)
 logger.info(f"Selected data index saved to {save_file}")
@@ -184,7 +202,7 @@ desc_pool_stru = np.concatenate([np.mean(desc, axis=0).reshape(1,desc.shape[-1])
 pca_result = pca.fit_transform(desc_trn_stru) # PCA reduction for all data points
 trn_points = pca.transform(desc_trn_stru) 
 pool_points = pca.transform(desc_pool_stru) 
-select_points = pca.transform([desc_pool_stru[i] for i in range(len(desc_pool_stru)) if i in select_index])
+select_points = pca.transform([desc_pool_stru[i] for i in range(len(desc_pool_stru)) if i in RND_global_select_indices])
 
 
 plt.figure(figsize=(12, 8))
@@ -218,7 +236,7 @@ logger.info(f"PCA visualization saved to {pca_fig_name}")
 logger.info("Saving selected data points in dpdata format...")
 selected_dpdata = dpdata.MultiSystems()
 selected_string = ""
-for i in select_index:
+for i in RND_global_select_indices:
     keyname, ind = desc_pool_keys[i].split("-")
     # save keyname-ind pairs
     selected_string += f"{keyname}-{ind}\n"
