b02a4661abea7ea401a56e6f81464984fe00b83b
QuantumMisaka <ff6757442@gmail.com>
2025-09-15 09:38:11 +0800

    chore: 更新.gitignore并简化__init__.py
    添加test目录到.gitignore忽略列表
    移除__init__.py中的冗余代码和打印语句

--- src/dpeva/__init__.py
+++ src/dpeva/__init__.py
@@ -6,7 +6,7 @@ DPEVA: Deep Potential EVolution Accelerator
 from __future__ import annotations
 from importlib.metadata import PackageNotFoundError, metadata # version
 
-package_metadata = metadata("dpeva")
+__version__ = "0.2.0"
 
 try:
     __version__ = package_metadata.get("version")
@@ -14,8 +14,3 @@ except PackageNotFoundError:
     __version__ = "0.2.0-alpha"
 
 __author__ = "James Misaka"
-
-# from .sampling import clustering, direct, pca, stratified_sampling
-# from .uncertain import rnd, rnd_models
-
-print(f"Initializing DP-EVA version {__version__}")
